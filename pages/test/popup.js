dcb20ba7b036d38f1ba3a9791288ac98a3ec56f4
leetaeuk <kindly007@gmail.com>
2022-08-15 01:34:43 +0900

    변경

---
+++ pages/test/popup.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import * as DGB from '/component/common/DGB';
+
+export default function Popup(props, ref) {
+    return (
+        <>
+            팝업페이지
+        </>
+    )
+}
